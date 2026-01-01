f4d0702e0ce76612b5c971197d9241ca30a7042c
MustafaTalha26 <mustafatalhamert26@gmail.com>
2026-01-01 13:30:14 +0300

    Multi module

---
+++ order_exporter/src/test/java/com/example/order_exporter/OrderExporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.order_exporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderExporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ order_generator/src/test/java/com/example/order_generator/OrderGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.order_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
