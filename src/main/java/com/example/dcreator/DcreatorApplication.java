997482093f637c2be530981a7072cc1f864dbcd2
MustafaTalha26 <mustafatalhamert26@gmail.com>
2026-01-01 12:28:17 +0300

    First commit

---
+++ src/main/java/com/example/dcreator/DcreatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.dcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dcreator/DcreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dcreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
